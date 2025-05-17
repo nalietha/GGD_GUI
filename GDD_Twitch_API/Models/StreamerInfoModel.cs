8d9a61f7c5fc85c3869ec49f4550d605d9eab35b
Nathan Lietha <Nathan.Lietha@outlook.com>
2025-05-17 14:26:29 -0500

    Added TwtichAPI.dll to project, Initial calls put into GGD display project Updated names Again. Follow this convetion

---
+++ GDD_Twitch_API/Models/StreamerInfoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGDTwitchAPI.Models
+{
+    public class StreamerInfoModel
+    {
+
+        public string Name { get; set; } = string.Empty;// updated by the user. or the api call
+        public string ID { get; set; } = string.Empty;// Updated by the api call or by the user
+        public bool IsActive { get; set; } = false; // set by the web interface
+        public string Color { get; set; } = "#FFFFFF"; // Set by the user or white
+        public bool IsLive { get; set; } = false;    // Will be updated by the api calls
+    }
+
+}
--- GGD_Display/FileController.cs
+++ GGD_Display/FileController.cs
@@ -1,9 +1,20 @@
-﻿using System.Text.Json;
+﻿using System.Diagnostics;
+using System.Text.Json;
 
 namespace GGD_Display
 {
     public class FileController : DisplayModels
     {
         // moved Create Json file and Version controll to index.cs, TODO: add the rest of the file controller items here
+        var twitch = new TwitchApiService(Config.TwitchClientId, Config.TwitchClientSecret);
+        await twitch.InitializeAsync();
+
+        var streamers = LoadFromSaveFile(); // user-defined color, name, id
+        await twitch.UpdateStreamerData(streamers);
+        SaveToSaveFile(streamers);
+
+
+
+
     }
 }
